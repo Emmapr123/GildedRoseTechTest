f9795ecfe870d3cbfc101015790bf82ce75de05b
Emmapr123 <emmapriester@hotmail.com>
2021-05-05 16:03:32 +0100

    Created a new function in which backstage passes are updated

--- src/Item.js
+++ src/Item.js
@@ -14,7 +14,8 @@ class Shop {
     const MaxQuality = 50
     const MinQuality = 0
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[0].name == 'Aged Brie') { this.updateQualityAgedBrie() 
+      if (this.items[i].name == 'Aged Brie') { this.updateQualityAgedBrie() 
+      } else if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') { this.updateQualityBackstagePass() 
       } else {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > MinQuality) {
@@ -70,8 +71,30 @@ class Shop {
     if (this.items[0].quality < MaxQuality) {
       this.items[0].quality = this.items[0].quality += 1;
     }
-    
+
     this.items[0].sellIn -= 1;
+    return this.items
+  }
+  updateQualityBackstagePass() {
+    const MaxQuality = 50
+    console.log(this.items[0].quality)
+    if (this.items[0].quality < MaxQuality) {
+      this.items[0].quality = this.items[0].quality + 1;
+      if (this.items[0].sellIn < 11) {
+        if (this.items[0].quality < MaxQuality) {
+          this.items[0].quality = this.items[0].quality + 1;
+        }
+      }
+      if (this.items[0].sellIn < 6) {
+        if (this.items[0].quality < MaxQuality) {
+          this.items[0].quality = this.items[0].quality + 1;
+        }
+      }
+    }
+    this.items[0].sellIn -= 1;
+    if (this.items[0].sellIn < 0) {
+      this.items[0].quality = this.items[0].quality - this.items[0].quality;
+    }
     return this.items
   }
 }
