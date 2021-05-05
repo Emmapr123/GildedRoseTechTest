e74557c1b8364ddbc7609b713bc6e43f6481c694
Emmapr123 <emmapriester@hotmail.com>
2021-05-05 14:23:26 +0100

    Quality of a normal item goes down twice as fast when the sellIn date has passed

--- spec/ItemSpec.js
+++ spec/ItemSpec.js
@@ -10,10 +10,17 @@ describe("Gilded Rose", function() {
 });
 
 describe('A normal item with two days to sell and a quality of 10', function() {
+  const normalItem = new Shop([ new Item("Bagel", 2, 10)]);
+
   it('should go down by 1 quality point when 1 day has passed', function() {
-    const normalItem = new Shop([ new Item("Bagel", 2, 10)]);
     const items = normalItem.updateQuality();
     expect(items[0].quality).toEqual(9)
     expect(items[0].sellIn).toEqual(1)
+  })
+  it('once the sell by date has passed, quality degrades twice as fast', function() {
+    normalItem.updateQuality()
+    const items = normalItem.updateQuality()
+    expect(items[0].sellIn).toEqual(-1)
+    expect(items[0].quality).toEqual(6)
   })
 })
