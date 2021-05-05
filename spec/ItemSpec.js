98d0c21da0fb666ee5497b901165821d64b8236b
Emmapr123 <emmapriester@hotmail.com>
2021-05-05 14:15:44 +0100

    Small steps, commented out most of the updateQuality function. Wrote a test and got it to pass for the quality of a normal item going down

--- spec/ItemSpec.js
+++ spec/ItemSpec.js
@@ -3,7 +3,16 @@ describe("Gilded Rose", function() {
   it("should foo", function() {
     const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].name).toEqual("foo");
+
   });
 
 });
+
+describe('A normal item with two days to sell and a quality of 10', function() {
+  it('should go down by 1 quality point when 1 day has passed', function() {
+    const normalItem = new Shop([ new Item("Bagel", 2, 10)]);
+    const items = normalItem.updateQuality();
+    expect(items[0].quality).toEqual(9)
+  })
+})
