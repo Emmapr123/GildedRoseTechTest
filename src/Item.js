1ef30de3f6c434dd70d6452c6bb58d235f4cfff4
Emmapr123 <emmapriester@hotmail.com>
2021-05-05 18:20:34 +0100

    All functions use the right index to update the items

--- src/Item.js
+++ src/Item.js
@@ -14,32 +14,32 @@ class Shop {
   }
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name == 'Aged Brie') { this.updateQualityAgedBrie() 
-      } else if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') { this.updateQualityBackstagePass() 
+      if (this.items[i].name == 'Aged Brie') { this.updateQualityAgedBrie(i) 
+      } else if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') { this.updateQualityBackstagePass(i) 
       } else if (this.items[i].name == 'Sulfuras, Hand of Ragnaros') { return this.items
-      } else { this.updateQualityRegularItem() }
+      } else { this.updateQualityRegularItem(i) }
     }
    
     return this.items;
   }
-  updateQualityAgedBrie() {
-    if (this.items[0].quality < this.maxQuality) {this.increaseQuality(1);}
+  updateQualityAgedBrie(index) {
+    if (this.items[index].quality < this.maxQuality) {this.increaseQuality(1);}
     this.decreaseSellIn()
   }
-  updateQualityBackstagePass() {
-    if (this.items[0].quality < this.maxQuality) {
-      if (this.items[0].sellIn > 10) { this.increaseQuality(1); } 
-      else if (this.items[0].sellIn <= 5) { this.increaseQuality(3); } 
+  updateQualityBackstagePass(index) {
+    if (this.items[index].quality < this.maxQuality) {
+      if (this.items[index].sellIn > 10) { this.increaseQuality(1); } 
+      else if (this.items[index].sellIn <= 5) { this.increaseQuality(3); } 
       else { this.increaseQuality(2); }
     }
 
     this.decreaseSellIn()
     if (this.items[0].sellIn < 0) { this.setQualityToZero() }
   }
-  updateQualityRegularItem() {
+  updateQualityRegularItem(index) {
     this.decreaseSellIn()
-    if (this.items[0].quality > this.minQuality) { 
-      this.items[0].sellIn < 0 ? this.decreaseQuality(2) : this.decreaseQuality(1)
+    if (this.items[index].quality > this.minQuality) { 
+      this.items[index].sellIn < 0 ? this.decreaseQuality(2) : this.decreaseQuality(1)
     }
   }
   decreaseQuality(amount) {
