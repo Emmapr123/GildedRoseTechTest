d008cf2299a57aaa50e5130c4a007f1e2c05dc9d
Emmapr123 <emmapriester@hotmail.com>
2021-05-05 16:50:27 +0100

    Created a function that handles the decreasing of the quality and of that takes care of the increasing of quality

--- src/Item.js
+++ src/Item.js
@@ -18,32 +18,38 @@ class Shop {
       } else if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') { this.updateQualityBackstagePass() 
       } else if (this.items[i].name == 'Sulfuras, Hand of Ragnaros') { return this.items
       } else { this.updateQualityRegularItem() }
-  }
+    }
    
-  return this.items;
+    return this.items;
   }
   updateQualityAgedBrie() {
-    if (this.items[0].quality < this.maxQuality) {this.items[0].quality = this.items[0].quality += 1;}
+    if (this.items[0].quality < this.maxQuality) {this.increaseQuality(1);}
     this.items[0].sellIn -= 1;
   }
   updateQualityBackstagePass() {
     if (this.items[0].quality < this.maxQuality) {
-      if (this.items[0].sellIn > 10) { this.items[0].quality = this.items[0].quality + 1; 
-      } else if (this.items[0].sellIn < 6) { this.items[0].quality = this.items[0].quality + 3; 
-      } else { this.items[0].quality = this.items[0].quality + 2; }
+      if (this.items[0].sellIn > 10) { this.increaseQuality(1); } 
+      else if (this.items[0].sellIn < 6) { this.increaseQuality(3); } 
+      else { this.increaseQuality(2); }
     }
+
     this.items[0].sellIn -= 1;
     if (this.items[0].sellIn < 0) { this.setQualityToZero() }
-  }
-  setQualityToZero() {
-    this.items[0].quality = this.items[0].quality - this.items[0].quality;
   }
   updateQualityRegularItem() {
     if (this.items[0].quality > this.minQuality) { this.items[0].quality = this.items[0].quality - 1 }
     this.items[0].sellIn = this.items[0].sellIn - 1;
     if (this.items[0].sellIn < 0 && this.items[0].quality > this.minQuality) {
-          this.items[0].quality = this.items[0].quality - 1;
+          this.decreaseQuality(1)
     }
   }
+  decreaseQuality(amount) {
+    this.items[0].quality = this.items[0].quality - amount;
+  }
+  increaseQuality(amount) {
+    this.items[0].quality = this.items[0].quality + amount;
+  }
+  setQualityToZero() {
+    this.items[0].quality = this.items[0].quality - this.items[0].quality;
+  }
 }
-
